4ad15d52c767772c26a005a9b1f48fe4e04d71a5
hossainshofiq <ah731710@gmail.com>
2025-04-08 17:59:11 +0600

    copy paste component

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,7 +1,9 @@
+// comment added
 // src/components/Footer.jsx
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white p-4 text-center">
+      {/* footer line */}
       <p>© {new Date().getFullYear()} Todo App. All rights reserved.</p>
     </footer>
   );
