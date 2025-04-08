a2738f568de348de816eaf8e8555c89afca6dd80
hossainshofiq <ah731710@gmail.com>
2025-04-08 18:01:09 +0600

    update footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,12 +1,12 @@
 // comment added
 // src/components/Footer.jsx
 const Footer = () => {
-  return (
-    <footer className="bg-gray-800 text-white p-4 text-center">
-      {/* footer line */}
-      <p>© {new Date().getFullYear()} Todo App. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
+    return (
+      <footer className="bg-gray-800 text-white p-4 text-center">
+        {/* footer line */}
+        <p>© {new Date().getFullYear()} Todo App. All rights reserved.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
